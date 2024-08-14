351cd18f25761ee6219232cbd140f29daa943ce9
Pragnesh Rathod <39561106+PragneshRathod901@users.noreply.github.com>
2024-08-14 22:31:58 +0530

    Initialize project and  hero section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+        background: "rgb(var(--color-background) / <alpha-value>)",
+      },
     },
   },
   plugins: [],
